5b85aa323c4911621aced190099ef1a0eeada64c
vimarshdeo <vdeo1@asu.edu>
2017-05-29 13:02:06 -0700

    Word Break

---
+++ src/WordBreak.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class WordBreak {
+	public boolean wordBreak(String s, List<String> wordDict) {
+		boolean[] arr = new boolean[s.length()+1];
+		arr[0] = true;
+		for(int i=1;i<=s.length();i++){
+			for(int j=0;j<i;j++){
+				if(arr[j]==true && wordDict.contains(s.substring(j, i))){
+					arr[i] = true;
+				}
+			}
+		}
+		return arr[s.length()];
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WordBreak obj = new WordBreak();
+		List<String> wordDict = new ArrayList<>();
+		wordDict.add("leet");
+		wordDict.add("code");
+		String s = "leetcodes";
+		System.out.println(obj.wordBreak(s, wordDict));
+	}
+
+}
